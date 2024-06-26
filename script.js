ceda6e1eaf45f5aac9359be2e9b66c04d82e75f1
amineallali <amineallali9@gmail.com>
2024-06-26 15:15:29 +0100

    new file:   index.html 	new file:   script.js 	new file:   style.css

---
+++ script.js
@@ -0,0 +1,48 @@
+function Calculator(){
+  
+    function getNumber(){
+      
+    }
+    
+    function getOperator(){
+      
+    }
+    
+    
+    return (
+      <div className= "calculator">
+        
+        <div className="screen">0</div>
+        
+        <CalculatorButton value= "1" onClick={getNumber}/>
+        <CalculatorButton value= "2" onClick={getNumber}/>
+        <CalculatorButton value= "3" onClick={getNumber}/>
+        <CalculatorButton value= 'div' onClick={getOperator}/>
+        
+        <CalculatorButton value= "4" onClick={getNumber}/>
+        <CalculatorButton value= "5" onClick={getNumber}/>
+        <CalculatorButton value= "6" onClick={getNumber}/>
+        <CalculatorButton value= "*" onClick={getOperator}/>
+        
+        <CalculatorButton value= "7" onClick={getNumber}/>
+        <CalculatorButton value= "8" onClick={getNumber}/>
+        <CalculatorButton value= "9" onClick={getNumber}/>
+        <CalculatorButton value= "-" onClick={getOperator}/>
+        
+        <CalculatorButton value= "C" onClick={getOperator}/>
+        <CalculatorButton value= "0" onClick={getNumber}/>
+        <CalculatorButton value= "=" onClick={getOperator}/>
+        <CalculatorButton value= "+" onClick={getOperator}/>
+        
+      </div>
+    )
+}
+  
+  
+function CalculatorButton(props){
+    return (
+      <button onClick={props.onClick}>{props.value}</button>
+    )
+}
+  
+ReactDOM.render(<div className ="container"> <Calculator/> </div>, document.getElementById('root'));
